d2ac9fbe86efe60fc3b74e6bc17ffb6b91593d7f
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-12-04 00:58:26 +0500

    Adding Layout components and setting it to layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { HeaderComponent } from "./header/header.component";
+import { RouterOutlet } from "@angular/router";
+import { FooterComponent } from "./footer/footer.component";
+import { HorizontalSidebarComponent } from "./horizontal-sidebar/horizontal-sidebar.component";
+import { TwoColSidebarComponent } from "./two-col-sidebar/two-col-sidebar.component";
+import { AddStockComponent } from "./add-stock/add-stock.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [SidebarComponent, HeaderComponent, RouterOutlet, FooterComponent, HorizontalSidebarComponent, TwoColSidebarComponent, AddStockComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
---
+++ src/app/layout/two-col-sidebar/two-col-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-col-sidebar',
+  imports: [],
+  templateUrl: './two-col-sidebar.component.html',
+  styleUrl: './two-col-sidebar.component.css'
+})
+export class TwoColSidebarComponent {
+
+}
