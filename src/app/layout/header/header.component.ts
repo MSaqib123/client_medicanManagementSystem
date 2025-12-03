73af50c8c217ab2bca80f3a9dcc8923d1e10d7f7
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-12-04 01:23:21 +0500

    Refactor header component to implement AfterViewInit lifecycle and enhance dropdown functionality

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
 
+
+
+import { Component, AfterViewInit } from '@angular/core';
+declare var $: any;
+declare var feather: any;
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+
+    setTimeout(() => {
+
+      // Feather icons
+      if (typeof feather !== 'undefined') {
+        feather.replace();
+      }
+
+      // Bootstrap Dropdown
+      $('[data-bs-toggle="dropdown"]').dropdown();
+
+      // Mobile Sidebar Toggle
+      $('#mobile_btn').on('click', function () {
+        $('.sidebar').toggleClass('slide-nav');
+        $('.page-wrapper').toggleClass('slide-nav-toggle');
+      });
+
+    }, 300);  // wait for HTML + JS load
+  }
 }
+
